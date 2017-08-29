61592008be937b30f3414d4627e16cd93b77a2b9
Tanmoy Banik <imtanmoybanik@gmail.com>
2017-08-29 11:04:10 +0600

    cors added

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,12 +5,14 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 const helmet = require('helmet');
 const passport = require('passport');
+const cors = require('cors');
 
 
 
 
 
 module.exports = (app) => {
+    app.use(cors());
     app.use(compression());
     app.use(helmet());
     app.use(bodyParser.json());
